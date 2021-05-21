bdb409796ef03f4900023ad4ac8ba41d76d6791d
Mihail Marian <mihailthebuilder@gmail.com>
2021-05-21 20:28:42 +0100

    comments

--- base_convert.py
+++ base_convert.py
@@ -7,13 +7,21 @@ def bc(base, num):
     num_decreasing = num
     rebased_num = ""
 
+    # find the highest power for the base that's smaller than the number
     while upper_bound * base < num:
         upper_bound *= base
         upper_power += 1
 
+    # for each base power that's <= highest power...
     while upper_power >= 0:
+        # divmod gets the quotient (digit) and remainder (num_decreasing)
+        # num_decreasing is reassigned the remainder
         (digit, num_decreasing) = divmod(num_decreasing, upper_bound)
+
+        # quotient becomes the next digit in the rebased number
         rebased_num += str(digit)
+
+        # go one power lower
         upper_bound = upper_bound // base
         upper_power -= 1
 
